448564c51b67e9ad40fc7ae73d6c4e3759292376
zwbXighQog <50114456+ColeoptereMoroseDeVan@users.noreply.github.com>
2024-04-19 18:35:51 +0200

    Copie du fichier de reports Karate

--- run_karate.sh
+++ run_karate.sh
@@ -7,4 +7,4 @@ DOCKER_IMAGE="karate_laurichapp"
 docker build -t $DOCKER_IMAGE ./karate/
 
 # Lancer le conteneur Docker en spécifiant le paramètre d'environnement
-docker run --rm --network laurichapp_networks -e "BASE_URL=http://service-passerelle:8080/api" $DOCKER_IMAGE > result_test.txt
+docker run --rm --network laurichapp_networks -v $PWD/karate-test-result/:/app/target/karate-reports/ -e "BASE_URL=http://service-passerelle:8080/api" $DOCKER_IMAGE > result_test.txt
