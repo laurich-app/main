3dd1d0dc32f3727f2bf6b1e06dded590ed0d836f
Justin Lecas <justin.lecas@gmail.com>
2024-05-08 22:13:32 +0200

    [JLE] - Suppression du login docker-compose car le repo est public, pour prévenir de conflit

--- init.sh
+++ init.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Limité seulement à télécharger les images Docker
-docker login ghcr.io -u justin.lecas@gmail.com -p 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
 
 # Vérifier si l'OS est un macOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
